a425f231217a27703f92e5df24a55d4f72fbadd1
ykwongaq <ykwongaq@connect.ust.hk>
2023-10-14 23:26:11 +0800

    Add Log

--- src/main/java/wyk/bp/graph/Factor.java
+++ src/main/java/wyk/bp/graph/Factor.java
@@ -1,35 +1,33 @@
 package wyk.bp.graph;
 
 import org.nd4j.linalg.api.ndarray.INDArray;
-import org.nd4j.linalg.factory.Nd4j;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
-public class Factor implements FactorGraphNode {
+import wyk.bp.utils.Log;
 
-    protected final INDArray matrix;
+public class Factor implements FactorGraphNode {
+    protected final INDArray distribution;
     protected final List<Variable<?>> variables;
 
     public Factor(final List<Variable<?>> variables, final INDArray matrix) {
         Objects.requireNonNull(variables, "Given variables list cannot be null");
         Objects.requireNonNull(matrix, "Given matrix cannot be null");
         this.variables = variables;
-        this.matrix = matrix;
+        this.distribution = matrix;
     }
-
     public void moveAxis(int[] originDims, int[] targetDims) {
         if (originDims.length != targetDims.length) {
             throw new IllegalArgumentException("Origin and target dimension array should have same size");
         }
-        final int numDims = this.matrix.shape().length;
+        final int numDims = this.distribution.shape().length;
         if (Arrays.stream(originDims).anyMatch(originDim -> originDim < 0 || originDim > numDims)) {
-            throw new IllegalArgumentException("Invalid origin dimensions specified: " + Arrays.toString(originDims));
+            throw new IllegalArgumentException(Log.genLogMsg(this.getClass(), "Invalid origin dimensions specified: " + Arrays.toString(originDims)));
         }
         if (Arrays.stream(targetDims).anyMatch(targetDim -> targetDim < 0 || targetDim > numDims)) {
-            throw new IllegalArgumentException("Invalid target dimensions specified: " + Arrays.toString(targetDims));
+            throw new IllegalArgumentException(Log.genLogMsg(this.getClass(), "Invalid target dimensions specified: " + Arrays.toString(targetDims)));
         }
 
         int[] newOrders = new int[numDims];
@@ -37,31 +35,36 @@ public class Factor implements FactorGraphNode {
             final int targetDim = targetDims[idx];
             newOrders[targetDim] = originDims[idx];
         }
-        this.matrix.permutei(newOrders);
+        this.distribution.permutei(newOrders);
     }
     public List<Variable<?>> getVariables() {
         return this.variables;
     }
-    public INDArray getMatrix() {
-        return this.matrix;
+    public INDArray getDistribution() {
+        return this.distribution;
     }
     public boolean haveSameVariable(final Factor otherFactor) {
         return this.variables.equals(otherFactor.getVariables());
     }
     @Override
     public int hashCode() {
-        int result = this.matrix.hashCode();
+        int result = this.distribution.hashCode();
         for (Variable<?> variable : this.variables) {
             result = 31 * result + variable.hashCode();
         }
         return result;
     }
-
     @Override
     public boolean equals(Object otherObj) {
         if (this == otherObj) return true;
         if (otherObj == null || this.getClass() != otherObj.getClass()) return false;
         Factor otherFactor = (Factor) otherObj;
-        return this.matrix.equals(otherFactor.matrix) && this.variables.equals(otherFactor.variables);
+        return this.distribution.equals(otherFactor.distribution) && this.variables.equals(otherFactor.variables);
+    }
+
+    public static Factor factorProduct(final Factor factor1, final Factor factor2) {
+
+
+        return null;
     }
 }
---
+++ src/main/java/wyk/bp/utils/Log.java
@@ -0,0 +1,12 @@
+package wyk.bp.utils;
+
+public class Log {
+
+    public static String genLogMsg(final String className, final String message) {
+        return "[" + className + "]" + message;
+    }
+
+    public static String genLogMsg(final Class<?> clazz, final String message)  {
+        return Log.genLogMsg(clazz.getSimpleName(), message);
+    }
+}
