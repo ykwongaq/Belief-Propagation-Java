396d2b98f025360f91b61730f2f80d09131ce6b5
= <ykwongaq@connect.ust.hk>
2023-10-20 17:49:43 +0800

    Change Log

--- src/main/java/wyk/bp/utils/Log.java
+++ src/main/java/wyk/bp/utils/Log.java
@@ -3,7 +3,7 @@ package wyk.bp.utils;
 public class Log {
 
     public static String genLogMsg(final String className, final String message) {
-        return "[" + className + "]" + message;
+        return "[" + className + "] " + message;
     }
 
     public static String genLogMsg(final Class<?> clazz, final String message)  {
